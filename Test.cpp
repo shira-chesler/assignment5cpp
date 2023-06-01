a2bb9a2ceb91006a7a321167092285f5a833052f
shira-chesler <shira.chesler@gmail.com>
2023-06-01 03:49:09 -0400

    changes

--- Test.cpp
+++ Test.cpp
@@ -178,7 +178,7 @@ TEST_CASE("Test each iterator individually but bigger - checks all items were re
         MagicalContainer::PrimeIterator it = MagicalContainer::PrimeIterator(mc).begin();
         MagicalContainer::PrimeIterator endit = MagicalContainer::PrimeIterator(mc).end();
         std::vector<int> primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-        size_t index = 0;
+        std::size_t index = 0;
 
         while(it != endit) {
             CHECK(*it == primes[index]);
